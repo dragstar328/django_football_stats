5bbbf26ca80bad50e7a988b1f5233ccde432c8df
dragstar328 <dragstarclassic@gmail.com>
2018-06-06 16:05:08 +0900

    add game detail

--- mysite/record/urls.py
+++ mysite/record/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
   path('', views.PortalView.as_view(), name='record_index'),
   path('games/', views.GameIndexView.as_view(), name='game_list'),
+  path('games/<int:pk>', views.game_detail_view, name='game_detail'),
   path('rivals/', views.RivalIndexView.as_view(), name='rival_list'),
   path('players/', views.PlayerIndexView.as_view(), name='player_list'),
 ]
--- mysite/record/views.py
+++ mysite/record/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 from django.views import generic
@@ -10,6 +10,31 @@ class GameIndexView(generic.ListView):
 
   def get_queryset(self):
     return Game.objects.order_by('-id')
+
+def game_detail_view(request, pk):
+  game = get_object_or_404(Game, pk=pk)
+  statss = Stats.objects.filter(game=game)
+  objects = {'game': game, 'statss': statss, }
+
+  goals = 0
+  assists = 0
+  passes = 0
+  intercepts = 0
+  summary = {}
+  for stats in statss:
+    goals += stats.goals
+    assists += stats.assists
+    passes += stats.passes
+    intercepts += stats.intercepts
+
+  summary['goals'] = goals
+  summary['passes'] = passes
+  summary['intercepts'] = intercepts
+  summary['assists'] = assists
+
+
+  objects = {'game': game, 'statss': statss, 'summary': summary}
+  return render(request, 'record/game_detail.html', objects)
 
 
 class PortalView(generic.TemplateView):
