4f45bc11565971356b14472fb5172b256d0823ee
Your Name <you@example.com>
2018-06-09 20:54:21 +0900

    edit portal

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!936z*nud(1if&&6=*!jm6j0$$cwe8=260uxjrt8pusxk0s-54'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=(
+  os.path.join(BASE_DIR, "static"),
+)
